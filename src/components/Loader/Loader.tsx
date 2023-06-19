5bee180d6253d49fe2a9be629d934535fc6714ac
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-06-19 13:55:51 +0200

    Created loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,16 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { Loader as LoaderIcon } from "react-feather";
+import styles from "./loader.module.css";
 
 export const Loader: FC = () => {
-  return <div>Loading...</div>;
+  const [isTimeout, setIsTimeout] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsTimeout(true), 2000);
+  }, []);
+
+  return (
+    <div className={styles.animationContainer}>
+      {isTimeout ? <div>No description available</div> : <LoaderIcon className={styles.rotate} size={64} />}
+    </div>
+  );
 };
